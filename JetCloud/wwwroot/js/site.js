9953998cd7a3a3d939f1b1031f2dfaee2440b9ce
PETER MANZ <2013750@chester.ac.uk>
2023-04-13 14:13:46 +0100

    update form upload file form closebutton

--- JetCloud/wwwroot/js/site.js
+++ JetCloud/wwwroot/js/site.js
@@ -7,10 +7,22 @@
 function showForm_hideForm()
 {
     var x = document.getElementById("uploadFileForm");
+    var bg = document.getElementById("filesGrid");
+    var uploadBtn = document.getElementById("iconTitle");
+    var icon = document.getElementById("icon");
+    var closeBtn = document.getElementById("closeBtn");
 
     if (x.style.display === "none") {
         x.style.display = "block";
+        bg.style.filter = "blur(8px)";
+        uploadBtn.style.color = "#662582";
+        icon.style.color = "#662582";
+        closeBtn.style.display = "block";
     } else {
         x.style.display = "none";
+        bg.style.filter = "none";
+        uploadBtn.style.color = "#f5f5f5";
+        icon.style.color = "#f5f5f5";
+        closeBtn.style.display = "none";
     }
 }
